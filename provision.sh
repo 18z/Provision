28e9597e5ea3377e20e2fee9888ae4b02d52948d
deanboole <xspiritualx@gmail.com>
2014-11-22 16:43:49 +0000

    ADD provision.sh

---
+++ provision.sh
@@ -0,0 +1,13 @@
+apt-get update
+
+apt-get install -y vim git tig python-dev python-pip tmux mongodb
+
+git config --global user.name "name"
+git config --globla user.email "email"
+
+git clone https://github.com/deanboole/provision.git
+
+mv provision/tmux.conf /root/tmux.conf
+
+pip install virtualenv pymongo
+
