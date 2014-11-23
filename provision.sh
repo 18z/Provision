3b4eaf55100615e68bcf3672509d596463c38a2c
deanboole <xspiritualx@gmail.com>
2014-11-23 02:50:21 +0000

    ADD gitconfig to provision.sh

--- provision.sh
+++ provision.sh
@@ -2,12 +2,10 @@ apt-get update
 
 apt-get install -y vim git tig python-dev python-pip tmux mongodb
 
-git config --global user.name "name"
-git config --globla user.email "email"
-
 git clone https://github.com/deanboole/provision.git
 
-mv provision/tmux.conf /root/tmux.conf
+mv provision/tmux.conf /root/.tmux.conf
+mv provision/gitconfig /root/.gitconfig
 
 pip install virtualenv pymongo flask
 
