b0676c2a263d3caba9ff8058ff0b410422c3e2a2
deanboole <xspiritualx@gmail.com>
2014-11-23 02:57:23 +0000

    code polished

--- provision.sh
+++ provision.sh
@@ -1,11 +1,17 @@
-apt-get update
+# Exit immediately if a command exits with a non-zero status
+set -e
+# Show progress
+set -x
 
+# Package installation
+apt-get update
 apt-get install -y vim git tig python-dev python-pip tmux mongodb
 
+# Get and install files
 git clone https://github.com/deanboole/provision.git
-
 mv provision/tmux.conf /root/.tmux.conf
 mv provision/gitconfig /root/.gitconfig
 
-pip install virtualenv pymongo flask
+
+# pip install virtualenv pymongo flask
 
