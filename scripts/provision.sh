5033b62b132e13d2f5b9e62b5a121a3f5ba8cb28
KunYu Chen <deanboole@users.noreply.github.com>
2014-12-18 11:48:11 +0800

    fixed bugs

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -7,14 +7,14 @@ set -x
 
 # Package installation
 apt-get update
-apt-get install -y vim git tig python-dev python-pip tmux mongodb
+apt-get install -y vim tig python-dev python-pip tmux mongodb
 
 # Get and install files
 git clone https://github.com/deanboole/provision.git
-mv provision/.tmux.conf ${HOME}
-mv provision/.gitconfig /etc/gitconfig
-mv provision/.vimrc /etc/vim/vimrc
-mv provision/.bashrc /etc/bash.bashrc
+mv ../.tmux.conf ${HOME}
+mv ../.gitconfig /etc/gitconfig
+mv ../.vimrc /etc/vim/vimrc
+mv ../.bashrc /etc/bash.bashrc
 
 
 # pip install virtualenv pymongo flask
