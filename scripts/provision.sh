db351e4955f51cb066cbd1fa40b5a2ebe16b3a90
KunYu Chen <deanboole@users.noreply.github.com>
2014-12-18 11:19:53 +0800

    change location to place gitconfig

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -12,7 +12,7 @@ apt-get install -y vim git tig python-dev python-pip tmux mongodb
 # Get and install files
 git clone https://github.com/deanboole/provision.git
 mv provision/.tmux.conf /root/
-mv provision/.gitconfig /root/
+mv provision/.gitconfig /etc/gitconfig
 mv provision/.vimrc /etc/vim/vimrc
 mv provision/.bashrc /etc/bash.bashrc
 
