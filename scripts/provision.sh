17678a88e8490ba5f232adde77e6b0372a19b819
KunYu Chen <deanboole@users.noreply.github.com>
2014-12-24 14:01:17 +0800

    Update provision.sh
    use ln as default method to place configs.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -12,17 +12,17 @@ apt-get install -y vim tig python-dev python-pip tmux mongodb ctags
 # Get and install config files
 # git clone https://github.com/deanboole/Provision.git
 
-#ln -sf ${HOME}/Provision/.tmux.conf ${HOME}/.tmux.conf
-#ln -sf ${HOME}/Provision/.gitconfig /etc/gitconfig
-#ln -sf ${HOME}/Provision/.vimrc /etc/vim/vimrc
-#ln -sf ${HOME}/Provision/.bashrc /etc/bash.bashrc
-#ln -sf ${HOME}/Provision/.gitignore ${HOME}/.gitignore
+ln -sf ${HOME}/Provision/.tmux.conf ${HOME}/.tmux.conf
+ln -sf ${HOME}/Provision/.gitconfig ${HOME}/.gitconfig
+ln -sf ${HOME}/Provision/.vimrc ${HOME}/.vimrc
+ln -sf ${HOME}/Provision/.bashrc ${HOME}/.bashrc
+ln -sf ${HOME}/Provision/.gitignore ${HOME}/.gitignore
 
-cp ${HOME}/Provision/.tmux.conf ${HOME}/.tmux.conf
-cp ${HOME}/Provision/.gitconfig ${HOME}/.gitconfig
-cp ${HOME}/Provision/.vimrc ${HOME}/.vimrc
-cp ${HOME}/Provision/.bashrc ${HOME}/.bashrc
-cp ${HOME}/Provision/.gitignore ${HOME}/.gitignore
+#cp ${HOME}/Provision/.tmux.conf ${HOME}/.tmux.conf
+#cp ${HOME}/Provision/.gitconfig ${HOME}/.gitconfig
+#cp ${HOME}/Provision/.vimrc ${HOME}/.vimrc
+#cp ${HOME}/Provision/.bashrc ${HOME}/.bashrc
+#cp ${HOME}/Provision/.gitignore ${HOME}/.gitignore
 
 # Apply bash.bashrc
 # source /etc/bash.bashrc
