a7a63f79a677f00dcaeedd90f34ef0654458d995
deanboole <xspiritualx@gmail.com>
2015-10-09 03:53:24 +0000

    Added config files download

--- scripts/vim-install.sh
+++ scripts/vim-install.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 # Exit immediately if a command exits with a non-zero status
-set -e
+    set -e
 # Show progress
-set -x
+    set -x
 
+# Download config files from github
+    cd ${HOME}
+    git clone https://github.com/deanboole/Provision
 
 # Install config files
+    ln -sf ${HOME}/Provision/vim/vimrc ${HOME}/.vimrc
 
-ln -sf ${HOME}/Provision/vim/vimrc ${HOME}/.vimrc
-
-mkdir -p ${HOME}/.vim/colors
-ln -sf ${HOME}/Provision/vim/colors/vividchalk.vim ${HOME}/.vim/colors
+# Install color scheme
+    mkdir -p ${HOME}/.vim/colors
+    ln -sf ${HOME}/Provision/vim/colors/vividchalk.vim ${HOME}/.vim/colors
 
 
 # Vundle installation
-if [ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]; then
-    mkdir ${HOME}/.vim/bundle/
-    git clone https://github.com/gmarik/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
-fi
+    if [ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]; then
+        mkdir ${HOME}/.vim/bundle/
+        git clone https://github.com/gmarik/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
+    fi
 
 # Install vim plugin (for vundle)
-vim +PluginInstall +qall
+    vim +PluginInstall +qall
